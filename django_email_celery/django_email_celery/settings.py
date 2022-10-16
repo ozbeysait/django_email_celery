fd5752a8a0104ae2f1f988ad5274c1f69f24abca
ozbeysait <ozbeyysait@gmail.com>
2022-10-16 04:35:07 +0300

    update smtp and celery

--- django_email_celery/django_email_celery/settings.py
+++ django_email_celery/django_email_celery/settings.py
@@ -136,11 +136,11 @@ CELERY_BEAT_SCHEDULER = 'django_celery_beat.schedulers:DatabaseScheduler'
 
 # SMTP SETTINGS
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_USE_TLS=True
-EMAIL_HOST='smtp.gmail.com'
-EMAIL_PORT=587
-EMAIL_HOST_USER='ozbeyysait@gmail.com'
-EMAIL_HOST_PASSWORD='kloikffvrpnpfgwj'
-DEFAULT_FROM_EMAIL='Celery <ozbeyysait@gmail.com>'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = 'Celery <ozbeyysait@gmail.com>'
 
 # kloikffvrpnpfgwj
